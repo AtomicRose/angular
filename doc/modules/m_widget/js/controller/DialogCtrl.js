77c818d1ba7533e5d11386b177dc2dd425d30971
popoAOAOgroan <329565409@qq.com>
2016-09-02 17:39:34 +0800

    up dialog change

--- doc/modules/m_widget/js/controller/DialogCtrl.js
+++ doc/modules/m_widget/js/controller/DialogCtrl.js
@@ -1,3 +1,13 @@
-app.controller('DialogCtrl', ['$scope', function ($scope) {
-    
+app.controller('DialogCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    window.headerConfig={
+        enableHeader: true,
+        enableBack: false,
+        title: '弹出框'
+    };
+    window.footerConfig = {
+        enableFooter: true
+    };
+    $rootScope.$broadcast('setHeaderConfig', window.headerConfig);
+    $rootScope.$broadcast('setFooterConfig', window.footerConfig);
+
 }]);
